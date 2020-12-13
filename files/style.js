56d9b9fad0842abf755decc8b83148479324a9b8
Jyotika999 <Jyotika9september@gmail.com>
2020-12-13 23:51:22 +0530

    Added footer

--- files/style.js
+++ files/style.js
@@ -1,23 +1,80 @@
-window.addEventListener("keydown", function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+window.addEventListener("keydown", function(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    if(!audio) return;
-    audio.currentTime =0;
-    audio.play();
-    key.childNodes[1].classList.add('playing');
-    setTimeout(function(){
-        key.childNodes[1].classList.remove('playing');
-    }, 90);
-})
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.childNodes[1].classList.add('playing');
+        setTimeout(function() {
+            key.childNodes[1].classList.remove('playing');
+        }, 90);
+    })
+    /*
+    let about = document.getElementById('contacts');
+    about.addEventListener("click", function(){
+        document.getElementById('contact').style.visibility= "visible";
+        document.getElementById('btn').style.visibility = "visible";
+    })
 
-let about = document.getElementById('contacts');
-about.addEventListener("click", function(){
-    document.getElementById('contact').style.visibility= "visible";
-    document.getElementById('btn').style.visibility = "visible";
-})
+    document.getElementById('btn').addEventListener("click", function(){
+        document.getElementById('contact').style.visibility= "hidden";
+        this.style.visibility= "hidden";
+    })
+    */
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-document.getElementById('btn').addEventListener("click", function(){
-    document.getElementById('contact').style.visibility= "hidden";
-    this.style.visibility= "hidden";
-})
+TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
